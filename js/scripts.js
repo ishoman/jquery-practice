f482368ffd1dcb3097ad87e406bb0b3a7f792090
Isaac Shoman and William Chu <isaac@>
2018-03-19 16:14:27 -0700

    Add console log functionality, walrus images, click events.

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,32 @@
 $(function() {
   var userInputSentence = prompt("Please enter a sentence (no special characters):");
+    console.log(userInputSentence);
   var firstLastCharacter = userInputSentence.charAt(0).toUpperCase() + userInputSentence.charAt(userInputSentence.length-1).toUpperCase();
   var combineResult = function combine(firstLastCharacterCombine) {
     return firstLastCharacterCombine
   };
   var combineReverse = function reverse(firstLastCharacterReverse) {
     return firstLastCharacterReverse.charAt(firstLastCharacterReverse.length-1)+firstLastCharacterReverse.charAt(0);
+    console.log(combineReverse);
   };
 
   var combineInputReverse = function inputReverse(userInputSentence, firstLastCharacterReverse) {
     return userInputSentence + firstLastCharacterReverse.charAt(firstLastCharacterReverse.length-1)+firstLastCharacterReverse.charAt(0);
+    console.log(combineInputReverse);
   };
 
   var combineInputReverseDivide = function inputReverseDivide(userInputSentence, firstLastCharacterReverse) {
     return userInputSentence.charAt(Math.floor(userInputSentence.length/2)) + userInputSentence + firstLastCharacterReverse.charAt(firstLastCharacterReverse.length-1)+firstLastCharacterReverse.charAt(0);
+    console.log(combineInputReverseDivide)
   };
 
-  alert(combineResult(firstLastCharacter));
-  alert(combineReverse(firstLastCharacter));
-  alert(combineInputReverse(userInputSentence,firstLastCharacter));
-  alert(combineInputReverseDivide(userInputSentence,firstLastCharacter));
+  $(".walrus img").click(function(){
+    alert(userInputSentence);
+    console.log(userInputSentence);
+  });
+
+  $(".smoking-walrus img").click(function(){
+    alert(combineInputReverseDivide(userInputSentence,firstLastCharacter));
+    console.log(combineInputReverseDivide(userInputSentence,firstLastCharacter));
+  });
 });
