cfaca55060c5a6f933c2b4778ac8480b0d3d3742
Isaac Shoman and William Chu <isaac@>
2018-03-19 15:39:54 -0700

    Add the final functions to produce cipher that shows reversed sentence with middle character at beginning.

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,23 @@
 $(function() {
-  var userInputSentence = prompt("Please enter a sentence(no special characters):");
-  var firstCharacter = userInputSentence.charAt(0).toUpperCase();
-  var lastCharacter = userInputSentence.charAt(userInputSentence.length-1).toUpperCase();
-  var combineRevResult = function combineReverse(firstCharacter, lastCharacter) {
-    return lastCharacter + firstCharacter;
-  }
-  alert(combineRevResult);
+  var userInputSentence = prompt("Please enter a sentence (no special characters):");
+  var firstLastCharacter = userInputSentence.charAt(0).toUpperCase() + userInputSentence.charAt(userInputSentence.length-1).toUpperCase();
+  var combineResult = function combine(firstLastCharacterCombine) {
+    return firstLastCharacterCombine
+  };
+  var combineReverse = function reverse(firstLastCharacterReverse) {
+    return firstLastCharacterReverse.charAt(firstLastCharacterReverse.length-1)+firstLastCharacterReverse.charAt(0);
+  };
+
+  var combineInputReverse = function inputReverse(userInputSentence, firstLastCharacterReverse) {
+    return userInputSentence + firstLastCharacterReverse.charAt(firstLastCharacterReverse.length-1)+firstLastCharacterReverse.charAt(0);
+  };
+
+  var combineInputReverseDivide = function inputReverseDivide(userInputSentence, firstLastCharacterReverse) {
+    return userInputSentence.charAt(Math.floor(userInputSentence.length/2)) + userInputSentence + firstLastCharacterReverse.charAt(firstLastCharacterReverse.length-1)+firstLastCharacterReverse.charAt(0);
+  };
+
+  alert(combineResult(firstLastCharacter));
+  alert(combineReverse(firstLastCharacter));
+  alert(combineInputReverse(userInputSentence,firstLastCharacter));
+  alert(combineInputReverseDivide(userInputSentence,firstLastCharacter));
 });
